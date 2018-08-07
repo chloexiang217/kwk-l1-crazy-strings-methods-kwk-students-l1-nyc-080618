4020bb682e79e7416b7be62d0e92aaa540676e32
chloexiang217 <chloe.xiang217@gmail.com>
2018-08-07 14:49:25 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -3,7 +3,7 @@
 def crazy_strings(string_one, string_two)
   string_one = string_one.upcase.reverse
   string_two = string_two.swapcase.gsub("s", "z")
-  "#{string_one}, #{string_two}"
+  return "#{string_one}, #{string_two}"
 end
 
 crazy_string("Hello", "Friends")
