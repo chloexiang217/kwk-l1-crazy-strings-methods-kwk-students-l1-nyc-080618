4a8dbc997709650653bc43a81021214bddd1d8ed
chloexiang217 <chloe.xiang217@gmail.com>
2018-08-07 14:50:19 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,8 +1,8 @@
 # write your method here
 
-def crazy_strings(string_one, string_two)
+def crazy_strings(string_one,string_two)
   string_one = string_one.upcase.reverse
-  string_two = string_two.swapcase.gsub("s", "z")
+  string_two = string_two.swapcase.gsub("s","z")
   return "#{string_one}, #{string_two}"
 end
 
